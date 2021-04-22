1497b067628ac42f223cfedae987a415ede3934f
Torai Lin <torailin55@gmail.com>
2021-04-22 18:30:23 +0800

    hw3 complete

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,9 @@
 function reverse(str) {
-  
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[str.length - i - 1];
+  }
+  console.log(result);
 }
 
-reverse('hello');
+reverse("1abc2");
