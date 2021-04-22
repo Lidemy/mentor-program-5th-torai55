e9708c9d4d84d2cc4e5979e5c9e9f5f22ac75ca8
Torai Lin <torailin55@gmail.com>
2021-04-22 18:25:21 +0800

    hw2 complete

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,11 @@
 function capitalize(str) {
-  
+  if (
+    "a".charCodeAt(0) <= str.charCodeAt(0) &&
+    str.charCodeAt(0) <= "z".charCodeAt(0)
+  ) {
+    str[0] = String.fromCharCode(str.charCodeAt(0) + 32);
+  }
+  return str;
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize("a,hello"));
