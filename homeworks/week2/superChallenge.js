184121349fbc78ab8c65fd39d921024ae35c9be3
Torai Lin <torailin55@gmail.com>
2021-04-25 11:57:25 +0800

    superchallenge complete

--- homeworks/week2/superChallenge.js
+++ homeworks/week2/superChallenge.js
@@ -1,5 +1,12 @@
 function add(a, b) {
   let result = 0;
+  while ((a & b) != 0) {
+    let carry = (a & b) << 1;
+    let remain = a ^ b;
+    a = carry;
+    b = remain;
+  }
+  result = a | b;
 
   return result;
 }
