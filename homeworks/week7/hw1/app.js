13afc399b9427946422f52fd2ba863e994399559
Torai Lin <torailin55@gmail.com>
2021-05-26 01:23:23 +0800

    wk7 hw1 revise

--- homeworks/week7/hw1/app.js
+++ homeworks/week7/hw1/app.js
@@ -1,8 +1,8 @@
 // 加入標示
-function appendMark(div) {
+function appendMark(div, text) {
   if (div.querySelector('.invalid')) return
   const p = document.createElement('p')
-  p.innerText = '此項目為必填'
+  p.innerText = text
   p.classList.add('invalid')
   div.appendChild(p)
 }
@@ -20,6 +20,23 @@ function getNameValue(div) {
   return results.filter((res) => res)
 }
 
+// 檢查格式
+function checkformat(name, value) {
+  let regexRule
+  switch (name) {
+    case 'phone':
+      regexRule = /09[0-9]{8}/
+      break
+    case 'email':
+      regexRule = /^[A-Za-z0-9]+@[A-Za-z0-9]+(\.[A-Za-z0-9]+)*$/
+      break
+    default:
+      return true
+  }
+  if (value.search(regexRule) === -1) return false
+  return true
+}
+
 const form = document.querySelector('form')
 const btn = document.querySelector('.btn')
 
@@ -27,10 +44,10 @@ form.addEventListener('input', (e) => {
   // 沒輸入內容且為必填 就加標記
   const isRequired = e.target.parentNode.className.split(' ').includes('required')
   if (!(e.target.value.length) && isRequired) {
-    appendMark(e.target.parentNode)
+    appendMark(e.target.parentNode, '此項目為必填')
     return
   }
-  //  有輸入內容且標記存在 就移除標記
+  // 有輸入內容且標記存在 就移除標記
   const marker = e.target.parentNode.querySelector('.invalid')
   if (marker) {
     marker.remove()
@@ -44,18 +61,28 @@ btn.addEventListener('click', (e) => {
   // 取得所有必填的 name: value
   const inputs = Array.from(requiredList, (required) => getNameValue(required))
 
-  // 檢查所有必填
-  if (inputs.some((input) => input.length === 0)) {
-    inputs.forEach((input, index) => {
-      if (input.length === 0) {
-        appendMark(requiredList[index])
-      }
-    })
-    return
-  }
+  // 檢查所有必填欄位
+  let isPass = true
+  inputs.forEach((input, index) => {
+    if (input.length === 0) {
+      appendMark(requiredList[index], '此項目為必填')
+      isPass = false
+    }
+  })
 
-  // 都有填 則取得所有輸入並印出
-  const msg = Array.from(questionList, (question) => getNameValue(question))
-    .join('\n')
+  // 檢查格式
+  const msgList = Array.from(questionList, (question) => getNameValue(question))
+  msgList.forEach((item, index) => {
+    if (item.length === 0) return
+    const [name, value] = item[0].split(': ')
+    if (!checkformat(name, value)) {
+      appendMark(questionList[index], '格式錯誤')
+      isPass = false
+    }
+  })
+
+  // 檢查通過 則取得所有輸入並印出
+  if (!isPass) return
+  const msg = msgList.join('\n')
   alert(msg)
 })
