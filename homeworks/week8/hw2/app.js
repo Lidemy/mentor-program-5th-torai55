14956a99a78f05e8fd065ac7687d0a32aaf299ae
Torai Lin <torailin55@gmail.com>
2021-06-03 18:00:36 +0800

    hw3 complete plus hw1 & hw2 revise

--- homeworks/week8/hw2/app.js
+++ homeworks/week8/hw2/app.js
@@ -1,6 +1,10 @@
 const CLIENTID = 'r0me6woz37936skc3tsuviuipkp9mb'
-const ACCEPTHEADER = 'Accept: application/vnd.twitchtv.v5+json'
-const LIMIT = 5
+const ACCEPT = 'application/vnd.twitchtv.v5+json'
+const GAMELIMIT = 5
+const STREAMLIMIT = 20
+/* eslint-disable */
+let OFFSET = STREAMLIMIT
+/* eslint-enable */
 
 function callTwitchAPI(endPoint, callback) {
   const request = new XMLHttpRequest()
@@ -16,7 +20,7 @@ function callTwitchAPI(endPoint, callback) {
   request.onerror = (e) => console.log(e)
   request.open('GET', endPoint)
   request.setRequestHeader('Client-ID', CLIENTID)
-  request.setRequestHeader('Accept', ACCEPTHEADER)
+  request.setRequestHeader('Accept', ACCEPT)
   request.send()
 }
 
@@ -32,26 +36,27 @@ function getTopGames(limit, callback) {
   })
 }
 
-function getStreams(game, callback, offset = 0, options = []) {
+function getStreams(game, callback, limit = STREAMLIMIT, offset = 0, options = []) {
   game = encodeURIComponent(game)
-  const endPoint = `https://api.twitch.tv/kraken/streams/?limit=20&game=${game}&offset=${offset}`
+  const endPoint = `https://api.twitch.tv/kraken/streams/?limit=${limit}&game=${game}&offset=${offset}`
 
   callTwitchAPI(endPoint, (streams) => {
-    if (options.length) {
-      callback(streams, ...options)
-    } else {
-      callback(streams)
-    }
+    callback(streams, ...options)
   })
 }
 
 function renderContent(streams, clearOld = true) {
   const container = document.querySelector('.gamelist')
+  /* eslint-disable */
   if (clearOld) {
     container.textContent = ''
-    OFFSET = 20
+    OFFSET = STREAMLIMIT
+  } else {
+    OFFSET += STREAMLIMIT
   }
+  /* eslint-enable */
 
+  // update title
   const gamename = document.querySelector('.gamename')
   gamename.textContent = streams.streams[0].game
 
@@ -96,7 +101,7 @@ function renderLinks(games) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  getTopGames(LIMIT, (topGames) => {
+  getTopGames(GAMELIMIT, (topGames) => {
     renderLinks(topGames)
     getStreams(topGames[0], renderContent)
   })
@@ -107,35 +112,65 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 })
 
-// -----------------------------------------------------
-// challenge
-let OFFSET = 20
+// // -----------------------------------------------------
+// // challenge
+// // 可以把 challenge 底下全部取消註解，體驗新的功能
+// // 捲動到接近底部，再抓新頻道
+// function throttle(method) {
+//   clearTimeout(method._tId)
+//   method._tId = setTimeout(() => {
+//     method()
+//   }, 100)
+// }
 
-function throttle(method) {
-  clearTimeout(method._tId)
-  method._tId = setTimeout(() => {
-    method()
-  }, 100)
-}
+// function scrollHandler() {
+//   // DO NOT use anonymous function in throttle
+//   throttle(expandContent)
+// }
 
-function scrollHandler(event) {
-  // DO NOT use anonymous function in throttle
-  throttle(expandContent)
-}
+// // [bug] 排名變動時，可能會抓到重複的頻道
+// function expandContent() {
+//   const { scrollHeight, scrollTop, clientHeight } = document.documentElement
+//   const distanceToBottom = scrollHeight - scrollTop - clientHeight
 
-// [bug] 排名變動時，可能會抓到重複的頻道
-function expandContent() {
-  const { scrollHeight, scrollTop, clientHeight } = document.documentElement
-  const distanceToBottom = scrollHeight - scrollTop - clientHeight
+//   if (distanceToBottom <= 450) {
+//     const game = document.querySelector('.gamename').textContent
+//     const clearOld = false
 
-  if (distanceToBottom <= 450) {
-    const game = document.querySelector('.gamename').textContent
-    const clearOld = false
+//     getStreams(game, renderContent, STREAMLIMIT, OFFSET, [clearOld])
+//     if (OFFSET > 900) document.removeEventListener('scroll', scrollHandler)
+//   }
+// }
 
-    getStreams(game, renderContent, OFFSET, [clearOld])
-    OFFSET += 20
-    if (OFFSET > 900) document.removeEventListener('scroll', scrollHandler)
-  }
-}
+// document.addEventListener('scroll', scrollHandler)
 
-document.addEventListener('scroll', scrollHandler)
+// //-----------------------------------------------------
+// // 螢幕太大沒有卷軸（判斷高度再抓更多頻道）
+// // [bug] 若兩秒內抓不夠（例如伺服器回應延遲）就不會再抓
+// function loadMore() {
+//   const gameList = document.querySelector('.gamelist')
+//   const config = { attributes: true, childList: true, subtree: true }
+
+//   function mutateHandler() {
+//     const { clientHeight, scrollHeight } = document.documentElement
+//     if (clientHeight <= scrollHeight) {
+//       expandContent()
+//     }
+//   }
+
+//   const observer = new MutationObserver(mutateHandler)
+//   observer.observe(gameList, config)
+
+//   setTimeout(() => {
+//     observer.disconnect()
+//   }, 2000)
+// }
+
+// const links = document.querySelector('nav .links')
+// links.addEventListener('click', () => {
+//   loadMore()
+// })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   loadMore()
+// })
--- homeworks/week8/hw3.md
+++ homeworks/week8/hw3.md
@@ -1,14 +1,49 @@
 ## 什麼是 Ajax？
 
+[wiki](https://en.wikipedia.org/wiki/Ajax_(programming)) 上面有一張很棒的圖。
+
+![Ajax](https://upload.wikimedia.org/wikipedia/commons/0/0b/Ajax-vergleich-en.svg)
+
+全名為 Asynchronous JavaScript and XML。重點在那個非同步，讓網頁能夠一邊跟使用者進行互動，一邊與伺服器溝通的技術。等到伺服器做出回應，瀏覽器（JavaScript runtime）會自己找空檔（透過 event loop）把內容更新到網頁上。
+
+若是同步執行，網頁與伺服器溝通時，使用者就無法對網頁做操作。
 
 ## 用 Ajax 與我們用表單送出資料的差別在哪？
 
+利用表單送出資料是同步的方法。瀏覽器先使用 GET 從伺服器取得表單的頁面，使用者填完資料後，瀏覽器再用 POST 從伺服器取得新的頁面資料。取得新的頁面後再重新 render（server side render）。
+
+這兩個頁面間，如 nav bar 和 footer 或許就能共用，但伺服器還是整個頁面都放進 response，使效能較不好。使用者在送出表單，到瀏覽器 render 出新的頁面前也無法與這個網頁做其他互動。
+
+用 Ajax 送出 request 的話，可以只取需要更新部分的資料（如留言區），透過瀏覽器內的 JavaScript render 出頁面（client side render）。就不會中斷使用者的操作，也不會重複傳入整個頁面造成效能上的浪費。
 
 ## JSONP 是什麼？
 
+JSON with padding。顧名思義，就是 JSON 資料外面包了一層 padding。
+
+因為在瀏覽器上有個 [same origin policy](https://developer.mozilla.org/zh-TW/docs/Web/Security/Same-origin_policy)，瀏覽器會檢查 server response 的 header `Access-Control-Allow-Origin` 是否和發出 request 的網域 `origin` 相同，不同的話 response 會被瀏覽器擋下來不給網頁拿到（但 request 還是有成功發出去）。
+
+但 HTML 上有些標籤，本來就常會引用外部資源，不必遵守同源政策，例如 `<script>`、`<img>`。而 `<script>` 的內容又可以作為 JavaScript 程式碼使用，而衍生出了 JSONP 這個用法來獲取跨網域的資源。
+
+若使用有支持 JSONP 的 API，只要在 query string 中加入特定參數（根據 API 規範），那 API 會在回傳資料的外面包上你指定 function 名字的執行程式碼。
+
+例如這個 script 標籤會回傳使用者資料，帶上 `jsonp=parseResponse` 指定 function 的名字。
+
+```HTML
+<script src="http://server.example.com/RetrieveUser?UserId=1823&jsonp=parseResponse"></script>
+```
+
+瀏覽器最後所得到的結果，API 伺服器已經幫忙包好，變成 function call。只要在這網頁定義好 `parseResponse` 要做什麼，等資料回來就會自動執行了。
+
+```JavaScript
+parseResponse({"Name": "小明", "Id" : 1823, "Rank": 7})
+```
 
 ## 要如何存取跨網域的 API？
 
+如上，透過 JSONP 是一種方法。
+若是一般送 request，若 server 允許的話會自己附帶 `Access-Control-Allow-Origin: *`（或自己網域）的 response header，告訴瀏覽器允許 CORS。
+此時 request 也分成 [簡單請求與非簡單請求](https://developer.mozilla.org/zh-TW/docs/Web/HTTP/CORS#%E7%B0%A1%E5%96%AE%E8%AB%8B%E6%B1%82)，發送非簡單請求前，瀏覽器會自動發送一個 http method 為 OPTIONS 的 preflight request，向伺服器確認是否允許這網域存取。若否，瀏覽器就不會發送接下來的 request。
 
 ## 為什麼我們在第四週時沒碰到跨網域的問題，這週卻碰到了？
 
+同源政策這些，都是瀏覽器為了使用者安全所做出的限制。而第四週我們是在 Nods.js 上面執行 JavaScript 程式碼，所以在本地端執行時沒有瀏覽器的限制，自然就暢行無阻。用 curl 發 request 也是同樣的道理，只是工具不同。
